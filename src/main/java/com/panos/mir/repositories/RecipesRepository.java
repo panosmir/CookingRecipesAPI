f0aab4871e85589340cfac65cc67aea6220cc723
Panos <panagiotis.mirmigkis@gmail.com>
2017-04-15 14:49:53 +0300

    Uploaded the database file (with create schema)

--- src/main/java/com/panos/mir/repositories/RecipesRepository.java
+++ src/main/java/com/panos/mir/repositories/RecipesRepository.java
@@ -23,6 +23,8 @@ public interface RecipesRepository extends CrudRepository<Recipes, Integer>{
     @Query(value = "SELECT * FROM favorites, recipes, users WHERE favorites.user_id=users.user_id AND favorites.id=recipes.id", nativeQuery = true)
     List<Recipes> findFavorites();
 
+
+
 //    @Query(value = "INSERT INTO favorites(favorites.user_id, favorites.id) VALUES (?1, ?2)", nativeQuery = true)
 //    Recipes addFavorites(int id, int user_id);
 //    Remove comments in order to work the android app.
