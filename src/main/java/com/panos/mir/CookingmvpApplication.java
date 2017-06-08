512d8a9087204acf1bfd0596ed7fded4a2e78b98
Panos <panagiotis.mirmigkis@gmail.com>
2017-06-08 17:35:24 +0300

    Add "/" page to show the endpoints

--- src/main/java/com/panos/mir/CookingmvpApplication.java
+++ src/main/java/com/panos/mir/CookingmvpApplication.java
@@ -2,6 +2,9 @@ package com.panos.mir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class CookingmvpApplication {
