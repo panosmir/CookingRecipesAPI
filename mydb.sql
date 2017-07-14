dbf0b950429269e89560cd882c97391bedb0d73a
Panos <panagiotis.mirmigkis@gmail.com>
2017-07-14 15:14:00 +0300

    Add mydb schema file and insertions

--- mydb.sql
+++ mydb.sql
@@ -18,14 +18,105 @@ USE `mydb`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'kreas'),(2,'laxanika'),(3,'frouta');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `favorites`
+--
+
+DROP TABLE IF EXISTS `favorites`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `favorites` (
+  `user_id` int(11) NOT NULL,
+  `recipes_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`recipes_id`),
+  KEY `fk_users_has_recipes_recipes1_idx` (`recipes_id`),
+  KEY `fk_users_has_recipes_users1_idx` (`user_id`),
+  CONSTRAINT `FKk7du8b8ewipawnnpg76d55fus` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `fk_users_has_recipes_recipes1` FOREIGN KEY (`recipes_id`) REFERENCES `recipes` (`recipes_id`),
+  CONSTRAINT `fk_users_has_recipes_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `favorites`
 --
 
 LOCK TABLES `favorites` WRITE;
 /*!40000 ALTER TABLE `favorites` DISABLE KEYS */;
-INSERT INTO `favorites` VALUES (1,1),(1,2),(1,3),(2,1),(3,2);
+INSERT INTO `favorites` VALUES (1,1),(1,3),(2,3);
 /*!40000 ALTER TABLE `favorites` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `ingredients`
+--
+
+DROP TABLE IF EXISTS `ingredients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ingredients` (
+  `ingredients_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ingredient` varchar(45) DEFAULT NULL,
+  `category_id` int(11) NOT NULL,
+  `quantity` varchar(5) DEFAULT NULL,
+  PRIMARY KEY (`ingredients_id`,`category_id`),
+  KEY `fk_ingredients_category1_idx` (`category_id`),
+  CONSTRAINT `FK44j0v4g89hhdoe09s8fp7kos6` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`),
+  CONSTRAINT `fk_ingredients_category1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ingredients`
+--
+
+LOCK TABLES `ingredients` WRITE;
+/*!40000 ALTER TABLE `ingredients` DISABLE KEYS */;
+INSERT INTO `ingredients` VALUES (1,'kimas mosxarisions',1,NULL),(2,'kimas xoirinos',1,NULL),(3,'fileto kotopoulo',1,NULL),(4,'karoto',2,'20g'),(5,'piperia',2,NULL),(6,'kremmidi',2,'30g'),(7,'Spanaki',2,NULL),(8,'ntomata',3,NULL),(9,'portokali',3,NULL),(10,'mpanana',3,NULL);
+/*!40000 ALTER TABLE `ingredients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recipes`
+--
+
+DROP TABLE IF EXISTS `recipes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recipes` (
+  `recipes_id` int(11) NOT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`recipes_id`,`user_id`),
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `FKlc3x6yty3xsupx80hqbj9ayos` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `recipes`
@@ -33,9 +124,55 @@ UNLOCK TABLES;
 
 LOCK TABLES `recipes` WRITE;
 /*!40000 ALTER TABLE `recipes` DISABLE KEYS */;
-INSERT INTO `recipes` VALUES (1,'recipe 1 ','recipe 1 description',1),(2,'recipe 2 ','recipe 2 description',1),(3,'recipe 3 ','recipe 3 descriptiojn',2),(4,'recipe 4 ','recipe 4 description',2),(5,'spanakorizo','oraiotato spanakorizo!!!!!!',2),(6,'carbonara','carbonara italianoooo!!!!!!',12),(7,'fakes','pare fakes gia na ma8eis!!!!!',12),(8,'revithia','ta kalitera stin makedonia',12),(21,'kotopoulo','kotopoulaki!!!!!!',12),(22,'tiropita','i kaliteri tiropita sti makedonia',3),(23,'kimadopita','ela kosme na fas',3),(24,'omeleta','omeleta me loukanika!!!!!!',12),(26,'mpougatsa me krema','I kaliteri mpougatsa sti makedonia!!!',12),(43,'oti na nai','I kaliteri mpougatsa sti makedonia!!!',12),(44,'oti na na2i','I kaliteri mpougatsa sti makedonia!!!',12),(46,'kati kalo ','pw pw pw',5),(47,'spanakotiropita','i kaliteri',5),(49,'Kotopoulo','prwteini',5),(50,'test','test',5),(51,'pastitsio','the bestesest',5),(52,'prasopita','ela kosme na fas',12),(53,'cookies','ela kosme na fas',5),(54,'mpla mpla mpla','dfasdfasdfas',5);
+INSERT INTO `recipes` VALUES (1,'kimadopita','ela kosme na fas',1),(3,'ceasars salad','i kaliteri salata',2),(891,'Recipe from Mobile','jiasnfdijaksbdfpa;sdfa',1),(1788,'Test recipe','dfawdfasdfadsfa',1);
 /*!40000 ALTER TABLE `recipes` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `recipes_has_ingredients`
+--
+
+DROP TABLE IF EXISTS `recipes_has_ingredients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recipes_has_ingredients` (
+  `recipes_id` int(11) NOT NULL,
+  `ingredients_id` int(11) NOT NULL,
+  `quantity` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`recipes_id`,`ingredients_id`),
+  KEY `fk_recipes_has_ingredients_ingredients1_idx` (`ingredients_id`),
+  KEY `fk_recipes_has_ingredients_recipes1_idx` (`recipes_id`),
+  CONSTRAINT `FK2nsvs00tq3dqq2c17nop4afw7` FOREIGN KEY (`recipes_id`) REFERENCES `recipes` (`recipes_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK97cvcqe1omd2r30wg4cvmh9e5` FOREIGN KEY (`ingredients_id`) REFERENCES `ingredients` (`ingredients_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_recipes_has_ingredients_ingredients1` FOREIGN KEY (`ingredients_id`) REFERENCES `ingredients` (`ingredients_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_recipes_has_ingredients_recipes1` FOREIGN KEY (`recipes_id`) REFERENCES `recipes` (`recipes_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recipes_has_ingredients`
+--
+
+LOCK TABLES `recipes_has_ingredients` WRITE;
+/*!40000 ALTER TABLE `recipes_has_ingredients` DISABLE KEYS */;
+INSERT INTO `recipes_has_ingredients` VALUES (1,1,'300g'),(3,4,'150g'),(3,5,'80g'),(3,8,'180g'),(891,6,'30g'),(1788,1,'200g'),(1788,4,'20g'),(1788,6,'30g');
+/*!40000 ALTER TABLE `recipes_has_ingredients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
@@ -43,7 +180,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'test','test'),(2,'test1','test1'),(3,'test2','test2'),(5,'admin','admin'),(12,'test_panos','1234551');
+INSERT INTO `users` VALUES (1,'admin','admin'),(2,'test','test'),(3,'admin','admin');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -56,4 +193,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-04-07 19:05:14
+-- Dump completed on 2017-07-14 15:12:33
