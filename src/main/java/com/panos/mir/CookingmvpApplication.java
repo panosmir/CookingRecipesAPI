3d1425c51a41664cab6d30c6ea020897be7bcd88
panosmir <panagiotis.mirmigkis@gmail.com>
2018-12-02 19:16:26 +0200

    Code cleanup, https redirection

--- src/main/java/com/panos/mir/CookingmvpApplication.java
+++ src/main/java/com/panos/mir/CookingmvpApplication.java
@@ -3,7 +3,6 @@ package com.panos.mir;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
 public class CookingmvpApplication {
